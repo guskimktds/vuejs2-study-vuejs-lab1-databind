89f7d2b2b69f3d25e1c31bf6d631805bd1810429
Yongeon Kim <gusraccoon@gmail.com>
2020-02-03 19:22:33 +0900

    http request & route

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
-//const bus = new Vue();
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes,
+  mode:'history'
+});
+
+Vue.filter('snippet', function(value){
+  return value.slice(0,100) + '...';
+})
+
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
